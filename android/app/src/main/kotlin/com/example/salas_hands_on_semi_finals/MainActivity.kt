398c54750609b75f1249ba9862e218c01fe0bc88
Anthony <salasanthony515@gmail.com>
2022-12-19 21:20:25 +0800

    Commit Repository

---
+++ android/app/src/main/kotlin/com/example/salas_hands_on_semi_finals/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.salas_hands_on_semi_finals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+//import 'package:todo_list/todo_List.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Hands On Semi Final [Salas]',
+    theme: ThemeData(
+      primarySwatch: Colors.red,
+    ),
+    //home: const HomePage(),
+    )
+  );
+}
